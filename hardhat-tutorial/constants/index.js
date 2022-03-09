adf041cee33bbddd4ead76c2c1f8f3a359b62e26
am-miracle <judemiraco0@gmail.com>
2022-03-09 19:51:55 +0100

    update

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xcea6E761ddAcC6944A7867CD263b3626aF4879A0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "0x1E502fB076DB345Db4aB7D24fB4CfD8c45eefe62/api/";
+const METADATA_URL = "0x2A330B211F8DDbD6efD73C1Addf21AEc24C83b02/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
